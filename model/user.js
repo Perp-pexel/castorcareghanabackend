707615b2926755f807de63d2980b737605748b57
Perp-pexel <perpaman44@gmail.com>
2025-06-07 23:54:11 +0000

    user

--- model/user.js
+++ model/user.js
@@ -2,15 +2,16 @@ import { toJSON } from "@reis/mongoose-to-json";
 import { Schema, model } from "mongoose";
 
 const userSchema =new Schema({
-    name: {type: String, required:true },
+    firstName: {type: String, required:true },
+    lastName: { type: String, required:true },
     email: { type:String, required:true, unique:true },
     password: { type:String, required:true},
     // repeat_password: { type:String, ref:'password' },
     avatar: { type: String },
-    role: { type: String, default: 'user', enum: ['user', 'admin']}
-}); {
+    role: { type: String, default: 'user', enum: ['user', 'farmer', 'admin']}
+}, {
     timestamps: true
-}
+});
 
 userSchema.plugin(toJSON)
 
--- validator/user.js
+++ validator/user.js
@@ -1,7 +1,8 @@
 import Joi from "joi";
 
 export const registerUserValidator = Joi.object({
-    name: Joi.string().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     // repeat_password: Joi.string().required(),
@@ -9,14 +10,16 @@ export const registerUserValidator = Joi.object({
 });
 
 export const logInUserValidator = Joi.object({
-    name: Joi.string(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
 });
 
 
 export const updateProfileValidator = Joi.object({
-    name: Joi.string(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
     avatar: Joi.string(),
     password: Joi.string(), 
 });
