454e3b4e0b5acf1584bf0461292856da1788485d
Perp-pexel <perpaman44@gmail.com>
2025-06-10 21:58:52 +0000

    rbac

--- utils/rbac.js
+++ utils/rbac.js
@@ -2,14 +2,72 @@ export const permissions = [
     {
         role: 'user',
         actions: [
+            'createUser',
+            'updateUser',
+            'getUser',
+            'getUsers',
+            'getProduct',
+            'getProducts',
+            'getEducation',
+            'getEducations',
+            'createReview',
+            'updateReview',
+            'deleteReview',
+            'getReview',
+            'getReviews',
             
         ]
     }, 
+
+    {
+        role: 'farmer',
+        actions: [
+            'createUser',
+            'updateUser',
+            'getUser',
+            'getUsers',
+            'createProduct',
+            'updateProduct',
+            'getProduct',
+            'getProducts',
+            'deleteProduct',
+            'createEducation',
+            'updateEducation',
+            'deleteEducation',
+            'getEducation',
+            'getEducations',
+            'createReview',
+            'updateReview',
+            'deleteReview',
+            'getReview',
+            'getReviews'
+        ]
+    },
 
 
     {
         role: 'admin',
         actions: [
+            'createProduct',
+            'updateProduct',
+            'deleteProduct',
+            'getProduct',
+            'getProducts',
+            'createEducation',
+            'updateEducation',
+            'deleteEducation',
+            'getEducation',
+            'getEducations',
+            'createUser',
+            'updateUser',
+            'deleteUser',
+            'getUser',
+            'getUsers',
+            'createReview',
+            'updateReview',
+            'deleteReview',
+            'getReview',
+            'getReviews'
 
         ]
     }
