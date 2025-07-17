bf15b34db4c875ecfa325a2f29104a1b9de6e601
Perp-pexel <perpaman44@gmail.com>
2025-07-17 20:56:43 +0000

    update validator

--- validator/education.js
+++ validator/education.js
@@ -15,18 +15,20 @@ export const educationValidator = Joi.object({
   ).optional()
 });
 
+
 export const updateEducationValidator = Joi.object({
-  title: Joi.string().trim().min(3),
-  description: Joi.string().trim().min(10),
-  url: Joi.string().uri().allow(''),
-  fee: Joi.string(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  url: Joi.string().uri().allow(null, ''),         // URL is optional
+  fee: Joi.number().allow(null, ''),               // Fee is optional
   media: Joi.array().items(
     Joi.object({
-      type: Joi.string()
-        .valid('image', 'video', 'document', 'audio')
-        .required(),
-      filename: Joi.string(),
-      fileUrl: Joi.string().uri()
+      type: Joi.string().valid('image', 'video', 'audio', 'document').required(),
+      filename: Joi.string().required(),
+      fileUrl: Joi.string().uri().required()
     })
-  )
+  ).required() // Media is required (must be empty array if none)
 });
+
+
+
